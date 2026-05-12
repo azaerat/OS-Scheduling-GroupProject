fa78608b261474f77527f0e512c31b66c9bfb702
Destinyjr <89811194+JrDestiny@users.noreply.github.com>
2026-05-12 11:30:18 -0400

    FCFS.C

--- Program1.C
+++ Program1.C
@@ -1,71 +1,61 @@
 #include <stdio.h>
 
+struct Process {
+    int pid;
+    int arrival;
+    int burst;
+    int waiting;
+    int turnaround;
+};
+
 int main() {
+
     int n;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int arrival[n], burst[n];
+    struct Process p[n];
 
+    // Input
     for (int i = 0; i < n; i++) {
-        printf("Enter arrival time for P%d: ", i+1);
-        scanf("%d", &arrival[i]);
 
-        printf("Enter burst time for P%d: ", i+1);
-        scanf("%d", &burst[i]);
+        p[i].pid = i + 1;
+
+        printf("Enter arrival time for P%d: ", p[i].pid);
+        scanf("%d", &p[i].arrival);
+
+        printf("Enter burst time for P%d: ", p[i].pid);
+        scanf("%d", &p[i].burst);
     }
 
-    int waiting[n], turnaround[n];
+    // Sort by arrival time (FCFS)
+    for (int i = 0; i < n - 1; i++) {
+
+        for (int j = 0; j < n - i - 1; j++) {
+
+            if (p[j].arrival > p[j + 1].arrival) {
+
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+
     int time = 0;
 
+    // Calculate WT and TAT
     for (int i = 0; i < n; i++) {
-        if (time < arrival[i]) {
-            time = arrival[i];
+
+        // CPU idle case
+        if (time < p[i].arrival) {
+            time = p[i].arrival;
         }
 
-        waiting[i] = time - arrival[i];
-        time += burst[i];
-        turnaround[i] = waiting[i] + burst[i];
+        p[i].waiting = time - p[i].arrival;
+
+        time += p[i].burst;
+
+        p[i].turnaround = p[i].waiting + p[i].burst;
     }
-
-    printf("\nP\tAT\tBT\tWT\tTAT\n");
-
-    for (int i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\n",
-               i+1, arrival[i], burst[i], waiting[i], turnaround[i]);
-    }
-
-    
-    float totalWT = 0, totalTAT = 0;
-
-    for (int i = 0; i < n; i++) {
-        totalWT += waiting[i];
-        totalTAT += turnaround[i];
-    }
-
-    printf("\nAverage Waiting Time: %.2f", totalWT / n);
-    printf("\nAverage Turnaround Time: %.2f\n", totalTAT / n);
-
-    printf("\nGantt Chart:\n|");
-
-for (int i = 0; i < n; i++) {
-    printf(" P%d |", i+1);
-}
-
-printf("\n0");
-
-int currentTime = 0;
-
-for (int i = 0; i < n; i++) {
-    if (currentTime < arrival[i]) {
-        currentTime = arrival[i];
-    }
-    currentTime += burst[i];
-    printf("   %d", currentTime);
-}
-
-printf("\n");
-
-    return 0;
-}
